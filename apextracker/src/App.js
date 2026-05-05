6fc95adef78acc28e03b4e37a64155c2fb4275bf
szymonmar <szymon.marszaek@gmail.com>
2026-05-05 10:50:58 +0200

    basic routing added

--- apextracker/src/App.js
+++ apextracker/src/App.js
@@ -1,24 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+function Home() {
+  return <h1>Home</h1>;
+}
+
+function Schedule() {
+  return <h1>Schedule</h1>;
+}
+
+function Series() {
+  return <h1>Series</h1>;
+}
+
+function SeriesList() {
+  return <h1>Series List</h1>;
+}
+
+function SeriesDetail() {
+  return <h1>Series Detail</h1>;
+}
+
+function RoundDetail() {
+  return <h1>Round Detail</h1>;
+}
+
+function Live() {
+  return <h1>Live</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/schedule">Schedule</Link> |{" "}
+        <Link to="/series">Series</Link> |{" "}
+        <Link to="/series/formula-1">Formula 1</Link> |{" "}
+        <Link to="/round/1">Round 1</Link> |{" "}
+        <Link to="/round/2">Round 2</Link> |{" "}
+        <Link to="/live">Live</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/schedule" element={<Schedule />} />
+        <Route path="/series" element={<Series />}>
+          <Route index element={<SeriesList />} />
+          <Route path="/series/:slug" element={<SeriesDetail />} />
+        </Route>
+        <Route path="/round/:id" element={<RoundDetail />} />
+        <Route path="/live" element={<Live />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
