a9e85624a23264366380f6ad79955471ec2433c8
Mautyn <127203976+Mautyn@users.noreply.github.com>
2026-06-08 10:44:55 +0200

    Replace Firebase config with environment variables

--- apextracker/src/firebase/register.js
+++ apextracker/src/firebase/register.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyDiJr7eDYgzZsmL_uC3o28jljLP01vN-dk',
-    authDomain: 'apextracker-dffd4.firebaseapp.com',
-    projectId: 'apextracker-dffd4',
-    storageBucket: 'apextracker-dffd4.firebasestorage.app',
-    messagingSenderId: '589323777509',
-    appId: '1:589323777509:web:07ddb4771ffd0be7d26b0c',
-    measurementId: 'G-3YT0Y3BPZ2'
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
