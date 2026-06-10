eac6d5ed2bff3b5f1560beecdad9190ad18b56fd
piotrek1kons <piotrek1kons@gmail.com>
2026-06-10 15:11:32 +0200

    hotjar

--- apextracker/src/index.js
+++ apextracker/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { hotjar } from 'react-hotjar';
+
+hotjar.initialize(871612, 6);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
